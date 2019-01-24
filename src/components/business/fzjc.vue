35e9db0641fc8593692c38529de47c101011ca1a
bai648080679 <31722548+bai648080679@users.noreply.github.com>
2019-01-24 09:31:06 +0800

    Add files via upload
    上传项目结构

---
+++ src/components/business/fzjc.vue
@@ -0,0 +1,8 @@
+<template>
+    <dir>辅助决策</dir>
+</template>
+<script>
+export default {
+    name: 'fzjc'
+}
+</script>
---
+++ src/components/synthetical.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="pie-box">
+        <div class="echarts-pie"></div>
+        <div class="echarts-pie"></div>
+        <div class="echarts-pie"></div>
+    </div>
+</template>
+<script>
+import '../assets/stylesheet/zhtsCss.css'
+export default {
+ name:'zhts'   
+}
+</script>
+
