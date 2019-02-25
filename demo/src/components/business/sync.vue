3534b593d66816ed0abba6bd85a5409f695a7d93
小白同学 <17600825103@163.com>
2019-02-25 16:57:19 +0800

    transition动画梳理

--- demo/src/components/business/sync.vue
+++ demo/src/components/business/sync.vue
@@ -1,5 +1,14 @@
 <template>
-    <div id="container" ref="contaIner" style='width:100%;height:100%'></div>
+    <div id="container" ref="contaIner" style='width:100%;height:100%'>
+        <button @click="show = !show">toggle</button>
+        <transition
+         name="fade"
+         enter-active-class="animated tada"
+         leave-active-class="animated slideinUp"
+         >
+            <p v-if="show">Holle</p>
+        </transition>
+    </div>
 </template>
 <style>
 #container{
@@ -12,12 +21,17 @@
 import $ from 'jquery'
 import * as echarts from 'echarts'
 import 'echarts-gl';
-console.log(echarts);
+import '../../assets/stylesheet/animation.css'
 export default {
     name: 'zhts',   
+   data() {
+       return {
+           show:true
+       }
+   },
     methods:{
         loadEchartsGL(){
-            
+           
         }
     },
     mounted: function(){
